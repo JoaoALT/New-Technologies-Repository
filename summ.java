eb1ee03a9285651e7cd37bd869759df30aab3824
Alex Muñoz <joaomuob@unisabana.edu.co>
2023-03-13 11:48:59 -0500

    added 3rd number to the summ method

--- summ.java
+++ summ.java
@@ -1,5 +1,5 @@
 public class summ {
-    public void suma(int num1,int num2){
-        System.out.println(num1+num2);
+    public void suma(int num1,int num2,int num3){
+        System.out.println(num1+num2+num3);
     }
 }
