e4cf194a6941876f6cdd904cba7bbaf83a057b82
BryamDigar <bryamdiaz1010@gmail.com>
2023-03-13 18:48:44 -0500

    Se crea funcion restar

--- CalculadoraBasica.java
+++ CalculadoraBasica.java
@@ -2,4 +2,13 @@ public class CalculadoraBasica {
     public int sumar(int numero, int numero2){
         return numero + numero2;
     }
+
+
+
+
+
+
+    public void restar(int numero, int numero2){
+        System.out.println(numero-numero2);
+    }
 }
